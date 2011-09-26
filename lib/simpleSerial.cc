b70915e99bbe509ba3cf724928c4404075f3e604
bromtimo <bromtimo@cse.msu.edu>
2011-09-26 18:32:18 -0400

    It seems to function at this point, beginning testing of how well it functions

--- lib/simpleSerial.cc
+++ lib/simpleSerial.cc
@@ -11,8 +11,16 @@ for read in file:
 #include <iostream>
 #include <stdlib.h>
 #include "parsers.hh"
+#include "counting.hh"
 
 using namespace std;
+using namespace khmer;
+
+#define K 32 /* kmer size */
+
+#define MAX_MEDIAN_COUNT 20 /* Only count if the median is <= this */
+
+/* Forward Declarations */
 
 int main(int argc, char **argv)
 {
@@ -24,15 +32,41 @@ int main(int argc, char **argv)
         exit(-1);
     }
 
-    /* For now, read in each read and just print it out */
-    FastaParser p(argv[1]);
+    /* Initialize the counting hash */
+    vector<HashIntoType> tableSizes;
+    tableSizes.push_back(100000000);
+    tableSizes.push_back(100000001);
+    tableSizes.push_back(100000002);
+    CountingHash h(K, tableSizes);
 
+    /* Start parsing the fasta file */
+    FastaParser p(argv[1]);
     Read r;
+    BoundedCounterType medCount;
+    float meanCount;
+    float stdDev;
+    long long unsigned int totalCount = 0, keptCount = 0;
 
     while (!p.is_complete())
     {
+        totalCount++;
         r = p.get_next_read();
-        cout << r.name << endl;
-        cout << r.seq << endl;
+        h.get_median_count(r.seq, medCount, meanCount, stdDev);
+        if (medCount <= MAX_MEDIAN_COUNT)
+        {
+            keptCount++;
+            /* Count it */
+            KMerIterator kmers(r.seq.c_str(), K);
+            while(!kmers.done())
+            {
+                h.count(kmers.next());
+            }
+
+            /* Save it to an output file */
+            
+        }
     }
+
+    printf("Total Count: %llu, Kept Count: %llu\n", totalCount, keptCount);
+    printf("Hashtable Occupancy: %lf\n", h.n_occupied() / 100000000.0);
 }
