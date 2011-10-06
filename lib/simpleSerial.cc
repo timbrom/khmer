324cf83829b0bab7bf3f1e73b003ac7a3b9bfffc
bromtimo <bromtimo@cse.msu.edu>
2011-10-06 12:36:39 -0400

    Added class for calculating prime numbers (primes.hh)
    Updated simpleSerial.cc to correctly use prime numbers for the hash functions. Also added functionality to print statistics at intervals.

--- lib/simpleSerial.cc
+++ lib/simpleSerial.cc
@@ -9,34 +9,59 @@ for read in file:
 */
 
 #include <iostream>
+#include <fstream>
 #include <stdlib.h>
+#include <stdio.h>
 #include "parsers.hh"
 #include "counting.hh"
+#include "primes.hh"
+#include <math.h>
 
 using namespace std;
 using namespace khmer;
 
-#define K 32 /* kmer size */
-
-#define MAX_MEDIAN_COUNT 20 /* Only count if the median is <= this */
+#define MAX_MEDIAN_COUNT 10 /* Only count if the median is < this */
+#define TABLE_SIZE 2000000000
 
 /* Forward Declarations */
 
 int main(int argc, char **argv)
 {
+    int numReads;
+    int numKeptReads;
+
     /* Check commandline arguments */
-    if (argc != 2)
+    if (argc != 4)
     {
-        cout << "You must specify a filename" << endl;
-        cout << "\tUsage: " << argv[0] << " filename" << endl;
+        cerr << "You must specify an input and output filename and the k-size" << endl;
+        cerr << "\tUsage: " << argv[0] << " input filename output filename k-size" << endl;
         exit(-1);
     }
 
+    ofstream outFile;
+    outFile.open(argv[2]);
+    if (! outFile.is_open())
+    {
+        cerr << "Failed to open file " << argv[2];
+        perror("");
+        cerr << endl;
+        exit(-1);
+    }
+
+    unsigned int K;
+    if (sscanf(argv[3], "%d", &K) != 1)
+    {
+        cerr << "Could not interpret K size of " << argv[3] << endl;
+        exit(-1);
+    }
+    
+
     /* Initialize the counting hash */
     vector<HashIntoType> tableSizes;
-    tableSizes.push_back(100000000);
-    tableSizes.push_back(100000001);
-    tableSizes.push_back(100000002);
+    Primes pri(TABLE_SIZE);
+    tableSizes.push_back(pri.get_next_prime());
+    tableSizes.push_back(pri.get_next_prime());
+    tableSizes.push_back(pri.get_next_prime());
     CountingHash h(K, tableSizes);
 
     /* Start parsing the fasta file */
@@ -47,14 +72,27 @@ int main(int argc, char **argv)
     float stdDev;
     long long unsigned int totalCount = 0, keptCount = 0;
 
+    numReads = 0;
+    numKeptReads = 0;
+    int numThousands = 1;
+    int numZeroCount;
+    unsigned long long avgMedCount = 0;
+
+    printf("ID\tNumKept  \tNumRead  \tNumZeroMed\tavgMedCnt\thtOccupancy\n");
+
     while (!p.is_complete())
     {
+        numReads++;
         totalCount++;
         r = p.get_next_read();
         h.get_median_count(r.seq, medCount, meanCount, stdDev);
-        if (medCount <= MAX_MEDIAN_COUNT)
+        avgMedCount += medCount;
+        if (medCount < MAX_MEDIAN_COUNT)
         {
             keptCount++;
+            numKeptReads++;
+            if (medCount == 0)
+                numZeroCount++;
             /* Count it */
             KMerIterator kmers(r.seq.c_str(), K);
             while(!kmers.done())
@@ -63,10 +101,23 @@ int main(int argc, char **argv)
             }
 
             /* Save it to an output file */
-            
+            outFile << ">" << r.name << endl;
+            outFile << r.seq << endl;
+        }
+
+        if (numReads == 1000000)
+        {
+            printf("%-4d\t%-10d\t%-10d\t%-10d\t%-10.2lf\t%-10lf\n", numThousands, 
+                numKeptReads, numReads, numZeroCount, 
+                (double)avgMedCount / (double)numReads,
+                (double)h.n_occupied() / TABLE_SIZE);
+            numThousands++;
+            numKeptReads = numReads = numZeroCount = avgMedCount = 0;
         }
     }
 
+    outFile.close();
+
     printf("Total Count: %llu, Kept Count: %llu\n", totalCount, keptCount);
-    printf("Hashtable Occupancy: %lf\n", h.n_occupied() / 100000000.0);
+    printf("Hashtable Occupancy: %lf\n", (double)h.n_occupied() / TABLE_SIZE);
 }
